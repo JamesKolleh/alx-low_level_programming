21b47e354d219c1e8ce61ff0855017e689881989
james Kolleh <jameskolleh@gmail.com>
2021-06-18 04:28:45 -0700

    added puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * prints the phrase "Programming is like buillding a multilingual puzzle
- *program should end with 0
+ *Return:0
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
