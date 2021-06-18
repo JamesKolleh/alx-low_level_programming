ed786783dcbe217cb7c73083e7094f6cd1261d70
james Kolleh <jameskolleh@gmail.com>
2021-06-18 04:56:07 -0700

    prints exactly with proper grammar

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *main-print exactly with proper grammar but the outcome
- *is a piece of art
- *Return:0
+ * main-print exactly with proper grammar but the outcome
+ * is a piece of art
+ * Return:0
  */
 int main(void)
 {
-printf("with proper grammar but the outcome is a piece af art")
+printf("with proper grammar but the outcome is a piece af art,\n");
 return (0);
 }
