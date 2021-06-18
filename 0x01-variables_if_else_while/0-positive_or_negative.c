cabf035493008494980031d58e621f145d981250
james Kolleh <jameskolleh@gmail.com>
2021-06-18 09:03:12 -0700

    fixing error

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - asigning random number to int
-
  * Return:0
  */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0);
-printf("%d is positive\n", n);
-else if (n == 0);
-printf("%d is zero\n", n);
-else if (n < 0);
-printf("%d is negative\n", n);
-return (0);
+       int n;
+
+       srand(time(0));
+       n = rand() - RAND_MAX / 2;
+       if (n > 0);
+               printf("%d is positive\n", n);
+       else if (n == 0);
+                printf("%d is zero\n", n);
+       else if (n < 0);
+                printf("%d is negative\n", n);
+       return (0);
 }
