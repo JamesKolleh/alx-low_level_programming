a7f220fbedbd8fec83f200d488edf97866973e25
james Kolleh <jameskolleh@gmail.com>
2021-06-19 05:34:26 -0700

    prints the alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio>
+
+/**
+ * main - prints the alphabet in lowercase,
+ *Return:0
+ */
+int main(void)
+{
+char ch;
+clrscr();
+for (ch= 'a'; ch<= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}
