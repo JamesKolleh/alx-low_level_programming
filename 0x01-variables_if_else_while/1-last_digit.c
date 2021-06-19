375a688a7b23a23c1721ebb6f041678cbd7c2177
james Kolleh <jameskolleh@gmail.com>
2021-06-19 02:29:34 -0700

    asigning random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,4 +1,4 @@
-#include <stdlid.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
@@ -21,3 +21,4 @@ printf("and is 0\n");
 else if (n % 10 < 6 && n % 10 != 0)
 printf("and is less than 6 and not 0\n");
 return (0);
+}
