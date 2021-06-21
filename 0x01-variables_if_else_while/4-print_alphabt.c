5b46644ab1667a0d2715d6f3f8bacb58f024532c
james Kolleh <jameskolleh@gmail.com>
2021-06-21 03:52:58 -0700

    print alphabet in lowercase excluding e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - print alphabet in lower case
+ * excluding e and q
+ * Return: 0
+ */
+int main(void)
+{
+char i = 'a';
+while (i = <= 'z')
+{
+if (i != 'e' && i != 'q')
+{
+putchar(i);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
