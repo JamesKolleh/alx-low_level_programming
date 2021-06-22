48375aa589e4092b15da0a04d07b4ba231361a5e
james Kolleh <jameskolleh@gmail.com>
2021-06-22 07:31:28 -0700

    print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - print alphabet
+ * Return:0
+ */
+void print_alphabet(void)
+{
+char ch;
+ch = 'a'
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
