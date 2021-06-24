72319fe2e6a01e5d479d61138d932f05ad411172
james Kolleh <jameskolleh@gmail.com>
2021-06-24 06:30:35 -0700

    added file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  * except 2 and 4, followed by a new line
  */
 void print_most_numbers(void)
-{  
+{
 int i;
 for (i = 0; i < 10; i++)
 {
-if (i != 2 && i != 4)	
-{  
-_putchar(i + '0');	  
+if (i != 2 && i != 4)
+{
+_putchar(i + '0')
 }
-}  
-_putchar('\n'); 
+}
+_putchar('\n');
 }
