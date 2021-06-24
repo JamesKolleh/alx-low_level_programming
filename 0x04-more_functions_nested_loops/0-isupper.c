282f64f0eb0a14db34d6845bc4958edd8d2c7e69
james Kolleh <jameskolleh@gmail.com>
2021-06-24 04:24:38 -0700

    checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks for upper case character
+ * @c: the character to be checked
+ * Return: 1
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
