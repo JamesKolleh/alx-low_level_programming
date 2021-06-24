84dd0ff8c49f819673157fe2c32a8e4a3bce3626
james Kolleh <jameskolleh@gmail.com>
2021-06-24 08:12:56 -0700

    print number from 1 - 100

--- 0x04-more_functions_nested_loops/9-fuzz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fuzz_buzz.c
@@ -1,12 +1,9 @@
-#include "holberton.h"
 #include <stdio.h>
 
 /**
- * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiples of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
- * Return: Always 0 (Success)
- */
+* main - prints the numbers from 1 - 100
+* Return: int
+*/
 int main(void)
 {
 int i;
@@ -14,22 +11,27 @@ for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 {
-printf(" Fizz");
+printf("Fizz");
 }
-else if (i % 5 == 0 && i % 3 != 0)
+else if (i % 3 != 0 && i % 5 == 0)
 {
-printf(" Buzz");
+printf("Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf(" FizzBuzz");
-}
-else if (i == 1)
-printf("%d", i);
+printf("FizzBuzz");
 }
 else
 {
-printf(" %d", i);
+printf("%d", i);
+}
+if (i != 100)
+putchar(' ');
+else
+putchar('\n');
+}
+return (0);
+}
 }
 }
 printf("\n");
