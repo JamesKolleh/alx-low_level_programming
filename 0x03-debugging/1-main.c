650b7bb908ab4b273af5f75363cbd4e023898b97
james Kolleh <jameskolleh@gmail.com>
2021-06-25 06:51:51 -0700

    causes an infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,9 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
 
 /*while (i < 10)*/
@@ -17,6 +19,7 @@ i = 0;
 /*}*/
 
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 
 }
